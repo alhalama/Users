9cb29babad12d6892f3aec64c36f1a128fe20c46
Alan Halama <alhalama@gmail.com>
2025-03-04 07:18:03 -0600

    Adding basic logging to Controller and Dependency Injection for IUserRepository

--- UsersApi/Controllers/UsersController.cs
+++ UsersApi/Controllers/UsersController.cs
@@ -15,16 +15,16 @@ public class UsersController : ControllerBase
 
     private readonly ILogger<UsersController> _logger;
 
-    public UsersController(ILogger<UsersController> logger)
+    public UsersController(ILogger<UsersController> logger, IUserRepository userRepository)
     {
         _logger = logger;
-        // TODO: dependency injection should be user here.
-        _userRepository = new InMemoryUserRepository();
+        _userRepository = userRepository;
     }
 
     [HttpGet(Name = "GetUsers")]
     public IEnumerable<User> Get()
     {
+        _logger.LogInformation("Get All Users");
        //foreach (User user in _users)
        foreach (User user in _userRepository.GetUsers())
        {
@@ -35,18 +35,21 @@ public class UsersController : ControllerBase
     [HttpGet("{id}")]
     public User Get(long id)
     {
+        _logger.LogInformation($"Get User for ID {id}");
         return _userRepository.GetUser(id);
     }
 
     [HttpPost()]
     public void Post(User user)
     {
+        _logger.LogInformation($"Adding a new user");
         _userRepository.AddUser(user);
     }
 
     [HttpPut("id")]
     public void Put(long id, User user)
     {
+        _logger.LogInformation($"Updating user with id {id}");
         // TODO: Replace user model with contract that doesn't have the ID.
         user.ID = id;
         _userRepository.UpdateUser(user);
@@ -55,6 +58,7 @@ public class UsersController : ControllerBase
     [HttpDelete("{id}")]
     public void Delete(long id)
     {
+        _logger.LogInformation($"Deleting user with id {id}");
         _userRepository.DeleteUser(id);
     }
 }
--- UsersApi/Program.cs
+++ UsersApi/Program.cs
@@ -1,6 +1,10 @@
+using UsersApi.Providers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddSingleton<IUserRepository, InMemoryUserRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
